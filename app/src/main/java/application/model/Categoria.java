3cc896e7b4f42d33d840320b044ee3234157c85a
joaogeremias <joaogeremias28@gmail.com>
2024-10-31 22:10:59 -0300

    add files again

--- app/src/main/java/application/model/Categoria.java
+++ app/src/main/java/application/model/Categoria.java
@@ -1,23 +1,49 @@
 package application.model;
 
-import java.lang.annotation.Inherited;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.perdsistence.Column;
-import jakarta.perdsistence.Entity;
-import jakarta.perdsistence.GeneratedValue;
-import jakarta.perdsistence.GeneretionType;
-import jakarta.perdsistence.Id;
-import jakarta.perdsistence.OneToMany;
-import jakarta.perdsistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "categorias")
 public class Categoria {
-@Id 
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private long id;
-@Column(unique = true, nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true, nullable = false)
+    private String nome;
 
+    @OneToMany(mappedBy = "categoria")
+    private Set<Jogo> jogos = new HashSet<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Jogo> getJogos() {
+        return jogos;
+    }
+
+    public void setJogos(Set<Jogo> jogos) {
+        this.jogos = jogos;
+    }
 }
